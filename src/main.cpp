c34f2d17c4a250a55ca8f09f8603a4946c19b7d3
Allan Nobre <allanjefrey9@hotmail.com>
2017-05-03 23:40:56 -0300

    Criando abstração para cenas.
    Signed-off-by: Hugo ALves <hugo.asb96@gmail.com>
    Signed-off-by: Allan Nobre <allanjefrey9@hotmail.com>
    Signed-off-by: Roger Lenke <rogerlenke@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "game.hpp"
 #include "game_globals.hpp"
+#include "player_scene.hpp"
 
 int main(int, char**) {
 	engine::Game game(
 		global::game_name,
 		global::window_width,
 		global::window_heigth);
+
+	PlayerScene player_scene;
+
+	game.AddScene(player_scene);
 
 	game.Run();
 	return 0;
