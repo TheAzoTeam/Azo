1cb17fa631aafcc9c94efe1f851f1238eb6072a2
Roger Lenke <rogerlenke@gmail.com>
2017-05-10 14:33:00 -0300

    Removendo código desnecessário, adicionando estados do componente.
    Signed-off-by: Allan Nobre <allanjefrey9@hotmail.com>
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>
    Signed-off-by: Roger Lenke <rogerlenke@gmail.com>

--- engine/src/component.cpp
+++ engine/src/component.cpp
@@ -1,15 +1,23 @@
 #include "component.hpp"
 
-engine::Component::Component(){
-}
+using namespace engine;
 
-engine::Component::~Component(){
-}
+Component::Component(){}
 
-bool engine::Component::Init(){
+Component::~Component(){}
+
+bool Component::Init(){
 	return true;
 }
 
-bool engine::Component::Shutdown(){
+bool Component::Shutdown(){
 	return true;
+}
+
+void Component::EnableComponent(){
+	component_state = State::ENABLED;
+}
+
+void Component::DisableComponent(){
+	component_state = State::DISABLED;
 }
