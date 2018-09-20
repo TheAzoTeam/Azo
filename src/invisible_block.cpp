2f964e82eb1c1d59e65d2e4fba309daeafa41fbd
Eduardo Lima <eduardolimrib@gmail.com>
2018-09-20 18:34:35 -0300

    Update comments for invisible_block.cpp.

--- src/invisible_block.cpp
+++ src/invisible_block.cpp
@@ -1,28 +1,53 @@
+/** 
+ * @file invisible_block.cpp
+ * @brief Purpose: Contains the InvisibleBlock class methods.
+ * 
+ * GPL v3.0 License
+ * Copyright (c) 2017 Azo
+ *
+ * https://github.com/TecProg2018-2/Azo/blob/master/LICENSE.md
+*/
 #include "invisible_block.hpp"
 
 using namespace Azo;
 
 InvisibleBlock::~InvisibleBlock(){}
 
+/**
+ * @brief Constructor for InvisibleBlock class.
+ * 
+ * Used for initializing class variables.
+ * @param name InvisibleBlock name.
+ * @param positionRelativeToParent Pair of doubles relative to position(range > 0).
+ * @param size Pair of doubles relative to size(range > 0).
+ */
 InvisibleBlock::InvisibleBlock(std::string name, std::pair<double, double> positionRelativeToParent,
 			       std::pair<double, double> size){
-	DEBUG("Creating Blocks");
+	DEBUG("Creating Blocks"); 
 
 	mName = name;
 	mPositionRelativeToParent = positionRelativeToParent;
 	mSize = size;
 
-	mHalfSize.first = mSize.first / 2.0f;
+	// 2.0f is used on the snippets below to halve the size while maintaining float type result.
+	mHalfSize.first = mSize.first / 2.0f; 
 	mHalfSize.second = mSize.second / 2.0f;
 
 	mCenter = mHalfSize;
 
+	// Initializing new ImageComponent with default invisible block image.
 	mImage = new engine::ImageComponent(*this, 
 										"backgrounds/test_invisible_1.png",
 										1);
 	this->addComponent(*mImage);
 }
 
+
+/**
+ * @brief Destructor class for InvisibleBlock.
+ * 
+ * Used for shutting down each ImageComponent so as to free memory when closing the game.
+ */ 
 void InvisibleBlock::shutdown(){
 	mImage->shutdown();
 	mImage = NULL;
