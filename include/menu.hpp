a008ae848eb04d30084d49f62d1868ea568d1374
KahCosta <kamillacosta.unb@gmail.com>
2018-10-01 11:47:02 -0300

    [Comment] on menu.hpp

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,3 +1,12 @@
+/**
+     * @file menu.hpp
+	 * @brief Purpose: Menu class declaration
+     *
+     * GPL 3.0 License
+     * Copyright (c) 2017 Azo
+     *
+     * https://github.com/TecProg2018-2/Azo/blob/master/LICENSE.md
+     */
 #ifndef MENU
 #define MENU
 
@@ -6,20 +15,27 @@
 #include "audio_controller.hpp"
 
 namespace Azo {
+	/**  
+		 * @brief A Hitbox class.
+		 * Menu class
+		 *
+		 * A more elaborate class description.
+		 * Class responsible for creating the ' menu '
+		 */
 	class Menu : public engine::Scene {
 		private:
-			engine::GameObject *mMenu;
+			engine::GameObject *mMenu; // Miscellaneous comment
 
 			engine::AnimationController *mAnimationController;
 
-			engine::Animation *mStartButton;
-			engine::Animation *mExitButton;
-			engine::Animation *mSoundEnabledButton;
-			engine::Animation *mSoundDisabledButton;
+			engine::Animation *mStartButton; // Variable referring to the Start button
+			engine::Animation *mExitButton; // Variable referring to the output button
+			engine::Animation *mSoundEnabledButton; // Variable referring to the Activate sound button
+			engine::Animation *mSoundDisabledButton; // Variable referring to the Activate sound button
 
-			engine::Animation *mArrowStart;
-			engine::Animation *mArrowExit;
-			engine::Animation *mArrowSound;
+			engine::Animation *mArrowStart; // Variable referring to start arrow
+			engine::Animation *mArrowExit; // Variable referring to output arrow
+			engine::Animation *mArrowSound; // Variable referring to sound
 
 			std::vector<engine::Sprite *> mStartButtonSprites;
 			std::vector<engine::Sprite *> mExitButtonSprites;
@@ -31,7 +47,7 @@ namespace Azo {
 			engine::BackgroundComponent *mBackground;
 			MenuCode *mCode;
 			engine::AudioController *mAudioController;
-			engine::AudioComponent *mMenuTheme;
+			engine::AudioComponent *mMenuTheme; // Variable referring to the menu
 
 		public:
 			Menu();
